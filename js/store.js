8d1691d4361f6357125411bb2adf58867f11ec8c
Antxny <antony.v94@outlook.com>
2021-06-14 14:10:05 +0200

    Correction d'un bug qui pouvait assigner 2 fois le même ID à la création d'un item
    Résolu en assignant la fonction Date.now() à l'ID à la création de l'item

--- js/store.js
+++ js/store.js
@@ -81,12 +81,7 @@
 		callback = callback || function () {};
 
 		// Generate an ID
-	    var newId = ""; 
-	    var charset = "0123456789";
-
-        for (var i = 0; i < 6; i++) {
-     		newId += charset.charAt(Math.floor(Math.random() * charset.length));
-		}
+	    var newId = Date.now();
 
 		// If an ID was actually given, find the item and update each property
 		if (id) {
