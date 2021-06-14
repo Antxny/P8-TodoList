820e1c1bd965a95b30576f99c47cbbc6b99b0c74
Antxny <antony.v94@outlook.com>
2021-06-14 14:05:57 +0200

    Correction d'une faute de frappe
    Faute de frappe dans controller.js:95 ( Controller.prototype.adddItem -> Controller.prototype.addItem )

--- js/controller.js
+++ js/controller.js
@@ -92,7 +92,7 @@
 	 * An event to fire whenever you want to add an item. Simply pass in the event
 	 * object and it'll handle the DOM insertion and saving of the new item.
 	 */
-	Controller.prototype.adddItem = function (title) {
+	Controller.prototype.addItem = function (title) {
 		var self = this;
 
 		if (title.trim() === '') {
